7f695c553a9c276328ee7b0fd9ba890c0646c678
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-05 04:52:05 +0700

    nested_array

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,24 @@
 //write your code here
+	var chessArr = [];
+	var catur = ['benteng','kuda','gajah','raja','ratu','gajah','kuda','benteng'];
+	var pion = 'pion';
+	var empty = 'empty';
+	for (var j = 0; j < 8; j++){
+		var tampung = []
+		for ( var i = 0; i < 8; i++){
+			if(j == 0 ){
+				tampung.push(catur[i] + ' Hitam');
+			}
+			else if( j == 7){
+				tampung.push(catur[i] + ' putih');
+			}else if( j == 1){
+				tampung.push(pion + ' Hitam')
+			}else if( j == 6){
+				tampung.push(pion + ' Putih');
+			}else{
+				tampung.push(empty);
+			}
+	}
+	chessArr.push(tampung)
+}
+console.log(chessArr[7][0])
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,21 @@
 //write your code here
+
+var table = [];
+var head = ['Number','Name','Position','Points perGame'];
+var baris1 = [12,'Joe Schmo','Center'[14, 32, 7 ,0 ,23]];
+var baris2 = [9, 'Ken buckets','poin Guard',[19, 0, 11, 22, 0]];
+var baris3 = [31, 'Harvey kay','Shooting Guard',[0, 30, 16, 0, 25]];
+var baris4 = [18, 'Sally Talls','Power Forward',[18, 29, 26, 31,19]];
+var baris5 = [22, 'Mo Diboux','Small Forward',[11, 0, 23, 17, 0]];
+
+function addData(data){
+	table.push(data)
+}
+addData(head);
+addData(baris1);
+addData(baris2);
+addData(baris3);
+addData(baris4);
+addData(baris5);
+
+console.log(table[3][2] == "Shooting Guard");		
