1f4db5733643ce2c373563fd676a93670ad612f9
Dale <dellryuzi@gmail.com>
2017-01-04 15:58:23 +0700

    done

--- chessboard.js
+++ chessboard.js
@@ -2,49 +2,44 @@
 let chess = ["blackPawn","blackKnight","blackBishop","blackRook","blackQueen","blackKing","whitePawn","whiteKnight","whiteBishop","whiteBishop","whiteQueen","whiteKing"];
 let chessBoard = [];
 
-
-
-
 for(let i=0; i<8;i++){
-var count = 9;
+var count =0;
+var tempBoard = [];
 
-  for(let j=0; j<8;j++){
-  //  while(count > 0 ){
-      if((i === 0 || i=== 7 )&& (j === 0 || j === 7 ){
-        chessBoard.push("blackRook");
+    while(count<8){
+      if(i===0 && (count === 0 || count === 7)){
+        tempBoard.push("blackRook");
+      }else if(i===0 && (count === 1 || count === 6)){
+        tempBoard.push("blackKnight");
+      }else if(i===0 && (count === 2 || count === 5)){
+        tempBoard.push("blackBishop");
+      }else if(i===0 && count === 4){
+        tempBoard.push("blackQueen");
+      }else if(i===0 && count === 3){
+        tempBoard.push("blackKing");
 
-      }else if((i === 0 || i=== 7 )&& (j === 1 || j === 6 ){
-        chessBoard.push("blackKnight");
+      }else if(i===1 ){
+        tempBoard.push("blackPawn");
+      }else if(i===6){
+        tempBoard.push("whitePawn");
 
-      }else if((i === 0 || i=== 7 )&& (j === 2 || j === 5 ){
-        chessBoard.push("blackBishop");
-
-      }else if((i === 0 || i=== 7 )&& (j === 4 ){
-        chessBoard.push("blackQueen");
-
-      }else if((i === 0 || i=== 7 )&& (j ===3 ){
-        chessBoard.push("blackKing");
-
-      }else if((i === 0 || i=== 7 )&& (j === 0 || j === 7 ){
-        chessBoard.push("whiteRook");
-
-      }else if((i === 0 || i=== 7 )&& (j === 1 || j === 6 ){
-        chessBoard.push("whiteKnight");
-      }else if((i === 0 || i=== 7 )&& (j === 0 || j === 5 ){
-        chessBoard.push("whiteBishop");
-      }else if((i === 0 || i=== 7 )&& (j === 4 ){
-        chessBoard.push("whiteQueen");
-      }else if((i === 0 || i=== 7 )&& (j === 3){
-        chessBoard.push("whiteQueen");
+      }else if(i===7 && (count === 0 || count === 7)){
+        tempBoard.push("whiteRook");
+      }else if(i===7 && (count === 1 || count === 6)){
+        tempBoard.push("whiteKnight");
+      }else if(i===7 && (count === 2 || count === 5)){
+        tempBoard.push("whiteBishop");
+      }else if(i===7 && count === 4){
+        tempBoard.push("whiteQueen");
+      }else if(i===7 && count === 3){
+        tempBoard.push("whiteKing");
       }
-      else if((i === 1 || i=== 6 ){
-        chessBoard.push("blackKnight");
-      }else if((i === 1 || i=== 6 ){
-        chessBoard.push("blackKnight");
-      }
-    //  count--;
-    
-  }
+        else break;
+      //console.log(i);
+      count++;
+    }
+
+  chessBoard.push(tempBoard);
 }
 
 console.log(chessBoard);
