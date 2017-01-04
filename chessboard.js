26dc891bd911373ca82f7ad63460daab210e7d47
Eri Irawan & Raditya Arya <pair+mrei91.radityaarya@hacktiv8.com>
2017-01-04 15:32:28 +0700

    done raditya_eri

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,35 @@
-//write your code here
+
+//write fnyour code here
+function chessBoard() {
+  let arrTemp       = [];
+  let rowColumn     = 8;
+  let anakCatur     = ['Benteng', 'Kuda', 'Menteri', 'Raja', 'Ratu', 'Menteri', 'Kuda', 'Benteng']
+  let pionCatur     = 'Pion';
+
+  for (let i = 0; i < rowColumn; i++) {
+    let arrIndex = [];
+     for (var j = 0; j < rowColumn; j++) {
+       if (i===0 || i ===7) {
+         if (i===0) {
+          arrIndex.push(anakCatur[j]+ ' hitam');
+         }
+         else {
+          arrIndex.push(anakCatur[j]+ ' putih');
+         }
+       }
+
+       else if (i===1 || i===6) {
+         if (i===1) {
+           arrIndex.push(pionCatur + ' hitam');
+         }
+         else {
+           arrIndex.push(pionCatur+ ' putih');
+         }
+       }
+      }
+    arrTemp.push(arrIndex);
+  }
+  return arrTemp;
+}
+
+ console.log(chessBoard()[7][0]); // benteng putih
