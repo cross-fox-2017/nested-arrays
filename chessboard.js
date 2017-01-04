bac3a6fbc65ded106031f8ee68fac2358070f549
Isumi & Muhammad Firman Pebrizal <pair+isumizumi.mfpebrizal@devbootcamp.com>
2017-01-04 16:44:49 +0700

    Done

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,32 @@
-//write your code here
+//Release0: chessboard
+var catur = ["Benteng","Kuda","Gajah","Raja","Ratu","Gajah","Kuda","Benteng"];
+var warna = ["Putih","Hitam"];
+var n = 8;
+var chessboard = [];
+
+for(var i=0; i<n; i++){
+  chessboard.push([]);
+}
+
+for(var j=0; j<n; j++){
+  chessboard[7].push(`${catur[j]} ${warna[0]}`);
+}
+
+for(var j=0; j<n; j++){
+  chessboard[0].push(`${catur[j]} ${warna[1]}`);
+}
+
+for(var j=0; j<n; j++){
+  chessboard[6].push(`Pion ${warna[0]}`);
+}
+
+for(var j=0; j<n; j++){
+  chessboard[1].push(`Pion ${warna[1]}`);
+}
+for(var i=2; i<6; i++){
+  for(var j=0; j<n; j++){
+    chessboard[i].push("Kosong");
+  }
+}
+
+console.log(chessboard[7][0]);
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,18 @@
-//write your code here
+//Release1: Spreadsheet
+let table = [];
+
+function addData (input) {
+	table.push(input);
+}
+
+let data1 = ["1", "John Doe", "Director", [10, 10, 10, 10]];
+let data2 = ["2", "Joe Schmo", "Center", [14, 32, 7, 0, 23]];
+let data3 = ["3", "Ken Buckets", "Point Guard", [9,0,11,2,0]];
+let data4 = ["4", "Harvey Kay", "Shooting Guard", [0,30,16,0,25]];
+addData(data1);
+addData(data2);
+addData(data3);
+addData(data4);
+console.log(table);
+console.log(table[3][2] == "Shooting Guard");
+console.log(JSON.stringify(table[1][3]) == JSON.stringify([14,32,7,0,23]));
