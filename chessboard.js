dae01e894d654dcd145c4e56fcf38bee6ca70b6d
Didit Suryadi &   "name": null <pair+didietsuryadi.wahyuhidayat@hacktiv8.com>
2017-01-04 16:30:47 +0700

    finish

--- chessboard.js
+++ chessboard.js
@@ -8,17 +8,34 @@ function chessboard(){
       var Pion      = "Pion";
       var Empty     = "Empty";
 
-      if(i === 0 || i == 7){
-        arrayChessboard.push(namaCatur);
+
+      if(i === 0){
+        arrayChessboard[i] = []
+        for(var o = 0; o < j; o++){
+        arrayChessboard[i].push(namaCatur[o] + " Hitam");
+        }
       }
-      else if (i == 1 || i == 6){
+      else if(i === 7){
         arrayChessboard[i] = []
         for(var k = 0; k < j; k++){
-          arrayChessboard[i].push(Pion);
+        arrayChessboard[i].push(namaCatur[k] + " Putih");
         }
-      }else{
-        arrayChessboard[i] = [];
+      }
+      else if (i == 1){
+        arrayChessboard[i] = []
         for(var l = 0; l < j; l++){
+          arrayChessboard[i].push(Pion + " Hitam");
+        }
+      }
+      else if(i == 6){
+        arrayChessboard[i] = []
+        for(var m = 0; m < j; m++){
+          arrayChessboard[i].push(Pion + " Putih");
+        }
+      }
+      else{
+        arrayChessboard[i] = [];
+        for(var n = 0; n < j; n++){
           arrayChessboard[i].push(Empty);
         }
 
@@ -30,3 +47,4 @@ function chessboard(){
 
 
 console.log(chessboard());
+console.log(chessboard()[7][0] == "Benteng Putih");
