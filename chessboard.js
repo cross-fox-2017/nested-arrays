56a3d0cd3f6c0c411fede05a8967462f03214cf5
timocodex <timothy.og.siahaan@gmail.com>
2017-01-04 17:21:19 +0700

    final nested array

--- chessboard.js
+++ chessboard.js
@@ -1,1 +1,34 @@
-//write your code here
+var chessboard = [];
+
+var catur=['Benteng','kuda','menteri','raja','ratu','menteri','kuda','benteng']
+
+for(var i=0; i<8; i++){
+  var h= [ ];
+    chessboard.push(h);
+}
+
+for(var i=0; i<chessboard.length;i++){
+  var d= ' ';
+  for(var j=0; j<chessboard.length;j++){
+
+  if(i===0){
+  chessboard[i].push(catur[j]+ " hitam");
+  }
+  else if(i===1){
+    chessboard[i].push("pion hitam");
+  }
+  else if(i===6){
+    chessboard[i].push("pion putih");
+  }
+  else if(i===7){
+    chessboard[i].push(catur[j]+" putih");
+  }
+  else{
+    chessboard[i].push(" ");
+  }
+}
+}
+
+
+
+console.log(chessboard[5][0]);
--- spreadsheet.js
+++ spreadsheet.js
@@ -1,1 +1,11 @@
-//write your code here
+var table =[];
+function addData(data){
+  table.push(data);
+}
+
+let judul= ['Number','Name','Position','Points per Game']
+let data = ['12','Joe Schmo','Center',[10,1,2,2]]
+let data2 =['9','Ken Buckets','Point Guard',[19,0,11,22,0]]
+addData(judul);
+addData(data);
+console.log(table[1][1]== 'Joe Schmo');
