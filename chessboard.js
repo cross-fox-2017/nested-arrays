5e29f4ada5590d078a7cb4e008ab10580855fd45
Ida Bagus Chahya Dhegana <dhegana@gmail.com>
2017-01-05 12:54:33 +0700

    Revisi chessboard dgana

--- chessboard.js
+++ chessboard.js
@@ -8,25 +8,25 @@ for(let i = 0; i < 8; i++){
   arrChessBoard.push(arr);
 }
 
-while(arrChessBoard[1].length < 9){
+while(arrChessBoard[1].length < 8){
   arrChessBoard[1].push('Pion Hitam');
 }
 
-while(arrChessBoard[6].length < 9){
+while(arrChessBoard[6].length < 8){
   arrChessBoard[6].push('Pion Putih');
 }
 
 for(let i = 2; i < 6; i++){
-  while(arrChessBoard[i].length < 9){
+  while(arrChessBoard[i].length < 8){
     arrChessBoard[i].push('');
   }
 }
 
-for(let i = 0; i < 9; i++){
+for(let i = 0; i < 8; i++){
   arrChessBoard[7][i] = bidak[i] + warna[1];
 }
 
-for(let i = 0; i < 9; i++){
+for(let i = 0; i < 8; i++){
   arrChessBoard[0][i] = bidak[i] + warna[0];
 }
 
